2260906d77c66b8ede25237daa36f006d47d91a8
Anurag Devanapally <anurag.devanapally@gmail.com>
2019-05-25 21:32:15 -0700

    Run Unity tests directly from bash script

---
+++ tools/run-unity.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -runEditorTests -projectPath /Users/travis/build/andeart/UnityLabs.SystemConsole/SystemConsole.Demo
