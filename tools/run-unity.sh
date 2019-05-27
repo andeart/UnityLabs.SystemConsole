570dc3cea50d3bf0b986376f19e966d7e3132c59
Anurag Devanapally <anurag.devanapally@gmail.com>
2019-05-26 20:31:30 -0700

    Update run-unity with activation

--- tools/run-unity.sh
+++ tools/run-unity.sh
@@ -1,12 +1,65 @@
 #! /bin/sh
 
 echo "Running Unity"
-export UNITY_EXECUTABLE=${UNITY_EXECUTABLE:-"/Applications/Unity/Unity.app/Contents/MacOS/Unity"}
+UNITY_PATH="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 
 cd /Users/travis/build/andeart/UnityLabs.SystemConsole/SystemConsole.Demo
 
+activateLicense() {
+    echo "Activating Unity..."
 
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} -batchmode -runEditorTests -projectPath $(pwd) -logFile $(pwd)/unity.log
+    ${UNITY_PATH} \
+        -logFile "${TRAVIS_BUILD_DIR}/unity.activation.log" \
+        -username ${UNITY_USER} \
+        -password ${UNITY_PWD} \
+        -batchmode \
+        -noUpm \
+        -quit
+    echo "Unity activation log"
+    cat "${TRAVIS_BUILD_DIR}/unity.activation.log"
+}
 
-echo "Logs from build"
-cat $(pwd)/unity.log
+returnLicense() {
+    echo "Return license"
+
+    ${UNITY_PATH} \
+        -logFile "${TRAVIS_BUILD_DIR}/unity.returnlicense.log" \
+        -batchmode \
+        -returnlicense \
+        -quit
+    echo "Unity return license log"
+    cat "$(pwd)/unity.returnlicense.log"
+}
+
+
+unitTests() {
+    echo "Running editor unit tests for ${UNITY_PROJECT_NAME}"
+
+    ${UNITY_PATH} \
+        -batchmode \
+        -username ${UNITY_USER} \
+        -password ${UNITY_PWD} \
+        -logFile "${TRAVIS_BUILD_DIR}/unity.unittests.log" \
+        -projectPath $(pwd) \
+        -runEditorTests \
+        -editorTestsResultFile "${TRAVIS_BUILD_DIR}/unity.unittests.xml"
+
+    rc0=$?
+    echo "Unit test log"
+    cat "${TRAVIS_BUILD_DIR}/unity.unittests.xml"
+
+    # exit if tests failed
+    if [ $rc0 -ne 0 ]; then { echo "Unit tests failed"; exit $rc0; } fi
+}
+
+oldUnitTests() {
+    ${UNITY_PATH} -batchmode -runEditorTests -projectPath $(pwd) -logFile $(pwd)/unity.log
+
+    echo "Logs from build"
+    cat $(pwd)/unity.log
+}
+
+activateLicense
+unitTests
+
+exit 0
