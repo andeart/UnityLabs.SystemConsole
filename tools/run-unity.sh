7537c4d568ea83af902e64895d16e1e1d308a46f
Anurag Devanapally <anurag.devanapally@gmail.com>
2019-05-26 18:09:49 -0700

    Update run-unity.sh

--- tools/run-unity.sh
+++ tools/run-unity.sh
@@ -1,13 +1,12 @@
 #! /bin/sh
 
+echo "Running Unity"
 export UNITY_EXECUTABLE=${UNITY_EXECUTABLE:-"/Applications/Unity/Unity.app/Contents/MacOS/Unity"}
 
 cd /Users/travis/build/andeart/UnityLabs.SystemConsole/SystemConsole.Demo
 
 
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity}
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} -batchmode -nographics -runEditorTests -projectPath $(pwd) -logFile $(pwd)/unity.log
 
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -runEditorTests -projectPath $(pwd) -logFile $(pwd)/unity.log
-
-echo 'Logs from build'
+echo "Logs from build"
 cat $(pwd)/unity.log
