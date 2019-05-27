7c74d0de707881937816814746506bb1d5027a9f
Anurag Devanapally <anurag.devanapally@gmail.com>
2019-05-26 18:22:32 -0700

    Remove -nographics from run-unity

--- tools/run-unity.sh
+++ tools/run-unity.sh
@@ -6,7 +6,7 @@ export UNITY_EXECUTABLE=${UNITY_EXECUTABLE:-"/Applications/Unity/Unity.app/Conte
 cd /Users/travis/build/andeart/UnityLabs.SystemConsole/SystemConsole.Demo
 
 
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} -batchmode -nographics -runEditorTests -projectPath $(pwd) -logFile $(pwd)/unity.log
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} -batchmode -runEditorTests -projectPath $(pwd) -logFile $(pwd)/unity.log
 
 echo "Logs from build"
 cat $(pwd)/unity.log
