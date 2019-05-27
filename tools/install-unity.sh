8715183cee84763d926689a6206ce09f272860b6
Anurag Devanapally <anurag.devanapally@gmail.com>
2019-05-26 20:46:55 -0700

    Update unity install/cache strategy

--- tools/install-unity.sh
+++ tools/install-unity.sh
@@ -4,20 +4,33 @@ BASE_URL=https://netstorage.unity3d.com/unity
 HASH=fc0fe30d6d91
 VERSION=2018.3.8f1
 
-download() {
-  file=$1
-  url="$BASE_URL/$HASH/$package"
+getFileName() {
+    echo "${UNITY_DOWNLOAD_CACHE}/`basename "$1"`"
+}
 
-  echo "Downloading from $url: "
-  curl -o `basename "$package"` "$url"
+download() {
+    file=$1
+    url="$BASE_URL/$HASH/$file"
+    filePath=$(getFileName $file)
+    fileName=`basename "$file"`
+
+    if [ ! -e $filePath ] ; then
+        echo "Downloading $filePath from $url: "
+        curl --retry 5 -o "$filePath" "$url"
+    else
+        echo "$fileName exists in cache. Skipping download."
+    fi
 }
 
 install() {
-  package=$1
-  download "$package"
+    package=$1
+    filePath=$(getFileName $package)
 
-  echo "Installing "`basename "$package"`
-  sudo installer -dumplog -package `basename "$package"` -target /
+    download "$package"
+
+    echo "Installing $filePath"
+    sudo installer -dumplog -package "$filePath" -target /
+
 }
 
 # See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
