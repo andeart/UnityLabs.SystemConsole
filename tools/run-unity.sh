20ca8e331ad6cd8bd083d0040cb3d0a711cfa63f
Anurag Devanapally <anurag.devanapally@gmail.com>
2019-05-26 17:54:49 -0700

    Update run-unity.sh to resolve license activation

--- tools/run-unity.sh
+++ tools/run-unity.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 
+export UNITY_EXECUTABLE=${UNITY_EXECUTABLE:-"/Applications/Unity/Unity.app/Contents/MacOS/Unity"}
+
 cd /Users/travis/build/andeart/UnityLabs.SystemConsole/SystemConsole.Demo
+
+
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity}
 
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -runEditorTests -projectPath $(pwd) -logFile $(pwd)/unity.log
 
